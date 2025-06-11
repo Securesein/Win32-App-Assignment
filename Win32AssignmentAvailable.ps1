23d2b573137fe38570c12d5ce32987e975547937
Sebas Smits <sebastiaan.smits@twentynice.com>
2025-06-11 14:44:31 +0200

    added parameters

--- Win32AssignmentAvailable.ps1
+++ Win32AssignmentAvailable.ps1
@@ -1,116 +1,190 @@
 function Set-Win32AppAssignmentAvailable {
     <#
     .SYNOPSIS
-        Sets the Win32 app assignment as Available (optional) for specified groups.
+        Assigns a Win32 application with the "Available" intent for targeted groups.
     
     .DESCRIPTION
-        Assigns a Win32 application with the "Available" intent, making it optional for enrolled devices in targeted groups.
-        This allows users to install the app from the Company Portal.
+        This function sets the assignment of a Win32 app as "Available" in the Company Portal.
+        It supports advanced assignment configuration including install time settings and restart options (for future-proofing).
     
     .LINK
         https://twentynice.com  
         https://www.linkedin.com/in/sebastiaan-smits-341513b6
     
     .NOTES
-        Version:        0.1
+        Version:        0.2
         Author:         Sebastiaan Smits
-        Creation Date:  2025-06-10
+        Creation Date:  2025-06-11
         File:           Set-Win32AppAssignmentAvailable.ps1
     
-    .PARAMETER AppID
-        Application ID (MobileAppId) of the Win32 application to assign.
-    
-    .PARAMETER MobileApp
-        Instead of AppID, you can provide the MobileApp object (type Microsoft.Graph.PowerShell.Models.MicrosoftGraphMobileApp).
-    
-    .PARAMETER groupID
-        One or more Azure AD Group Object IDs to assign the app as Available.
-    
-    .EXAMPLE
-        # Assign app with AppID to two groups as Available
-        $groups = @("5f1fc84e-53a0-4b3b-8346-xxxxxxx", "12f1de23-9999-4c3b-1244-yyyyyyy")
-        Set-Win32AppAssignmentAvailable -AppID "61e29a3b-fedd-423b-8409-xxxxxx" -groupID $groups
-    
-    .EXAMPLE
-        # Assign app by MobileApp object to one group as Available
-        $app = Get-MgDeviceAppManagementMobileApp -MobileAppId "61e29a3b-fedd-423b-8409-xxxxxx"
-        Set-Win32AppAssignmentAvailable -MobileApp $app -groupID "5f1fc84e-53a0-4b3b-8346-xxxxxxx"
-    
-    .LICENSE
+        LICENSE:
         Use this code free of charge at your own risk.
         Never deploy code into production if you do not know what it does.
+    
+    .PARAMETER AppID
+        Application ID (MobileAppId) of the Win32 app.
+    
+    .PARAMETER MobileApp
+        The MobileApp object (as returned by Get-MgDeviceAppManagementMobileApp).
+    
+    .PARAMETER groupID
+        One or more Azure AD Group Object IDs to assign the app to.
+    
+    .PARAMETER Note
+        Notification visibility setting (showAll, showReboot, hideAll). Default is showAll.
+    
+    .PARAMETER deliveryOptimizationPriority
+        Delivery optimization priority (notConfigured, foreground). Default is notConfigured.
+    
+    .PARAMETER useLocalTime
+        Whether to interpret start/deadline dates as local time (default is false).
+    
+    .PARAMETER startDateTime
+        UTC start date/time for install availability.
+    
+    .PARAMETER deadlineDateTime
+        UTC deadline date/time after which app is no longer available (optional).
+    
+    .PARAMETER restartGracePeriod
+        Switch to enable a grace period before device restarts (if enforced by app settings).
+    
+    .PARAMETER restartGracePeriodMinutes
+        Time in minutes for restart grace period (default 1440).
+    
+    .PARAMETER displayCountdownRestart
+        Time in minutes to show restart countdown (default 15).
+    
+    .PARAMETER allowSnooze
+        Whether to allow the user to snooze a pending restart.
+    
+    .PARAMETER snoozeDurationMinutes
+        Duration in minutes for each snooze (default 240).
+    
+    .EXAMPLE
+    $groups = @("group-id-1", "group-id-2")
+    Set-Win32AppAssignmentAvailable -AppID "app-id" -groupID $groups -Note showAll -startDateTime "2025-06-12T08:00:00.000Z"
     #>
     
-        [CmdletBinding(DefaultParameterSetName = "AppID")]
-        param (
-            [Parameter(Mandatory, ParameterSetName = "AppID", ValueFromPipeline)]
-            [Parameter(Mandatory, ParameterSetName = "MobileAppObject", ValueFromPipeline)]
-            [ValidateNotNullOrEmpty()]
-            [string]$AppID,
+    [CmdletBinding(DefaultParameterSetName = "AppID")]
+    param (
+        [Parameter(Mandatory, ParameterSetName = "AppID", ValueFromPipeline)]
+        [string]$AppID,
     
-            [Parameter(Mandatory, ParameterSetName = "MobileAppObject", ValueFromPipeline)]
-            [ValidateNotNullOrEmpty()]
-            [object]$MobileApp,
+        [Parameter(Mandatory, ParameterSetName = "MobileAppObject", ValueFromPipeline)]
+        [object]$MobileApp,
     
-            [Parameter(Mandatory)]
-            [ValidateNotNullOrEmpty()]
-            [string[]]$groupID
-        )
+        [Parameter(Mandatory)]
+        [string[]]$groupID,
     
-        begin {
-            function Test-Module {
-                param ([string]$ModuleName)
-                if (-not (Get-Module -ListAvailable -Name $ModuleName)) {
-                    Write-Host "Installing module $ModuleName"
-                    Install-Module -Name $ModuleName -Scope CurrentUser -Force
-                }
-                Import-Module -Name $ModuleName
+        [ValidateSet("showAll", "showReboot", "hideAll")]
+        [string]$Note = "showAll",
+    
+        [ValidateSet("notConfigured", "foreground")]
+        [string]$deliveryOptimizationPriority = "notConfigured",
+    
+        [switch]$useLocalTime,
+    
+        [ValidateScript({ [datetime]::ParseExact($_, "yyyy-MM-ddTHH:mm:ss.fffZ", $null) })]
+        [string]$startDateTime,
+    
+        [ValidateScript({ [datetime]::ParseExact($_, "yyyy-MM-ddTHH:mm:ss.fffZ", $null) })]
+        [string]$deadlineDateTime,
+    
+        [switch]$restartGracePeriod,
+    
+        [ValidateRange(1, 20160)]
+        [int]$restartGracePeriodMinutes = 1440,
+    
+        [ValidateRange(1, 240)]
+        [int]$displayCountdownRestart = 15,
+    
+        [switch]$allowSnooze,
+    
+        [ValidateRange(1, 712)]
+        [int]$snoozeDurationMinutes = 240
+    )
+    
+    begin {
+        function Test-Module {
+            param ([string]$ModuleName)
+            if (-not (Get-Module -ListAvailable -Name $ModuleName)) {
+                Write-Host "Installing module $ModuleName"
+                Install-Module -Name $ModuleName -Scope CurrentUser -Force
             }
-    
-            Test-Module -ModuleName "Microsoft.Graph.Devices.CorporateManagement"
-            Connect-MgGraph -Scopes "DeviceManagementApps.ReadWrite.All"
+            Import-Module -Name $ModuleName
         }
     
-        process {
-            # If MobileApp object provided, get its ID
-            if ($MobileApp -is [Microsoft.Graph.PowerShell.Models.MicrosoftGraphMobileApp]) {
-                $AppID = $MobileApp.ID
-            }
-            elseif ($MobileApp) {
-                throw "Invalid type for MobileApp parameter. Expected Microsoft.GraphMobileApp object."
-            }
+        Test-Module -ModuleName "Microsoft.Graph.Devices.CorporateManagement"
+        Connect-MgGraph -Scopes "DeviceManagementApps.ReadWrite.All"
+    }
     
-            # Build assignment objects for each group with intent "Available"
-            $assignments = @()
-            foreach ($id in $groupID) {
-                $assignment = [ordered]@{
-                    "@odata.type" = "#microsoft.graph.mobileAppAssignment"
-                    target = [ordered]@{
-                        "@odata.type" = "#microsoft.graph.groupAssignmentTarget"
-                        groupId = $id
-                    }
-                    intent = "Available"
-                    # For Available assignments, settings can be null or omitted
-                    settings = $null
-                }
-                $assignments += $assignment
-            }
+    process {
+        if ($MobileApp -is [Microsoft.Graph.PowerShell.Models.MicrosoftGraphMobileApp]) {
+            $AppID = $MobileApp.ID
+        }
+        elseif ($MobileApp) {
+            throw "Invalid type for MobileApp parameter. Expected Microsoft.GraphMobileApp object."
+        }
     
-            # Prepare request body
-            $appAssignmentHashtable = @{ mobileAppAssignments = $assignments }
-            $body = $appAssignmentHashtable | ConvertTo-Json -Depth 10
-    
-            # API endpoint URL - Beta endpoint is required for app assignment
-            $url = "https://graph.microsoft.com/beta/deviceAppManagement/mobileApps/$AppID/assign"
-    
-            # Send assignment request
-            try {
-                Invoke-MgGraphRequest -Uri $url -Body $body -Method POST -ContentType "application/json" | Out-Null
-                Write-Host "App assigned as Available to specified groups successfully."
-            }
-            catch {
-                Write-Error "Failed to assign app: $_"
+        $timeInstallSettings = if (-not $startDateTime -and -not $deadlineDateTime) {
+            $null
+        } else {
+            [ordered]@{
+                "@odata.type" = "#microsoft.graph.mobileAppInstallTimeSettings"
+                startDateTime = $startDateTime
+                deadlineDateTime = $deadlineDateTime
+                useLocalTime = $useLocalTime.ToString()
             }
         }
+    
+        if ($restartGracePeriod) {
+            $restartSettings = [ordered]@{
+                "@odata.type" = "#microsoft.graph.win32LobAppRestartSettings"
+                countdownDisplayBeforeRestartInMinutes = $displayCountdownRestart
+                gracePeriodInMinutes = $restartGracePeriodMinutes
+                restartNotificationSnoozeDurationInMinutes = $null
+            }
+        }
+    
+        if ($allowSnooze) {
+            $restartSettings = [ordered]@{
+                "@odata.type" = "#microsoft.graph.win32LobAppRestartSettings"
+                countdownDisplayBeforeRestartInMinutes = $displayCountdownRestart
+                gracePeriodInMinutes = $restartGracePeriodMinutes
+                restartNotificationSnoozeDurationInMinutes = $snoozeDurationMinutes
+            }
+        }
+    
+        $assignments = @()
+        foreach ($id in $groupID) {
+            $assignment = [ordered]@{
+                "@odata.type" = "#microsoft.graph.mobileAppAssignment"
+                target = [ordered]@{
+                    "@odata.type" = "#microsoft.graph.groupAssignmentTarget"
+                    groupId = $id
+                }
+                intent = "Available"
+                settings = [ordered]@{
+                    "@odata.type" = "#microsoft.graph.win32LobAppAssignmentSettings"
+                    notifications = $Note
+                    installTimeSettings = $timeInstallSettings
+                    restartSettings = $restartSettings
+                    deliveryOptimizationPriority = $deliveryOptimizationPriority
+                }
+            }
+            $assignments += $assignment
+        }
+    
+        $body = @{ mobileAppAssignments = $assignments } | ConvertTo-Json -Depth 10
+        $url = "https://graph.microsoft.com/beta/deviceAppManagement/mobileApps/$AppID/assign"
+    
+        try {
+            Invoke-MgGraphRequest -Uri $url -Method POST -Body $body -ContentType "application/json" | Out-Null
+            Write-Host "App successfully assigned as Available."
+        }
+        catch {
+            Write-Error "Failed to assign app: $_"
+        }
+    }
     }
     
